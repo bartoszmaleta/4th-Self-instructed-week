8ca30eae331d33ccbc73d599fa7141be08a96919
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-11-06 17:39:17 +0100

    Add sololearn module 2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Initial commit, exercises in 4th self instructed week
+
+https://www.sololearn.com/Course/Python/
+
---
+++ sololearn_module_2.py
@@ -0,0 +1,19 @@
+# Module 2: Control Structures
+
+# 20 * 12 / 2
+
+text = """First line 
+second line"""
+
+print(text)
+
+print(text * 0)
+
+num = 7
+if num > 3:
+    print("3")
+    if num < 5:
+        print("5")
+        if num == 7:
+            print("7")
+
